d35155b87eb4b685b7d1d73fc344d9736d380838
Coco <corentin.recanzone@epitech.eu>
2021-07-02 14:56:58 +0200

    Correctif : Remove import unsued + Gitignore

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './Views/Home';
 import FooterPage from './Components/BaliseSemantique/FooterPage';
 import './CSS/Router.css';
